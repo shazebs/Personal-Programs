c0b5e7e6515e111ec9418ecfcd568cffdb99eeec
shazebs <43024685+shazebs@users.noreply.github.com>
2021-02-27 15:20:29 -0800

    Add files via upload

---
+++ .py/spring2021/firstPythonTest.py
@@ -0,0 +1,27 @@
+# Shazeb Suhail's First Spyder Program.
+# 2.27.21 @ 3:11 pm 
+
+import time
+
+# this is how you print a line in python
+print("... Calculator ...\n")
+
+# this is my newline function in python
+def newline(n) :
+    # function variables
+    space = "\n"
+    print(n * space)
+# end of function newline
+
+print("first line!")
+newline(1)
+print("second line!")
+newline(1)
+print("third line!")   
+print("fourth line! (no newline prior)") 
+
+# the bottom is just some random test code
+print(time.clock_gettime(time))
+    
+        
+    
