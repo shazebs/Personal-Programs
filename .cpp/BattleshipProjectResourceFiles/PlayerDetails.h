71f21ad1ffa076b68b5559b54b6edf1a2a3e7bea
shazebs <43024685+shazebs@users.noreply.github.com>
2021-04-14 20:08:55 -0700

    Create PlayerDetails.h

---
+++ .cpp/BattleshipProjectResourceFiles/PlayerDetails.h
@@ -0,0 +1,18 @@
+class Ships
+{
+    // SHIP STRUCTURES.
+    struct CarrierShip
+    {   int carrier = 5; string coordinates[5] = {"X", "✈", "✈", "✈", "✈"};}; // 5 grid points
+    struct BattleShip
+    {   int battleship = 4;  string coordinates[5] = {"✈", "✈", "✈", "✈"};};  // 4 grid points
+    struct CruiserShip
+    {   int cruiser = 3;  string coordinates[5] = {"✈", "✈", "✈"};}; // 3 grid points
+    struct SubmarineShip
+    {   int submarine = 3;  string coordinates[5] = {"✈", "✈", "✈"};};  // 3 grid points
+    struct DestroyerShip
+    {   int destroyer = 2;  string coordinates[2] = {"✈", "✈"};};   // 2 grid points
+
+
+    // Private Instance Variables.
+
+};
