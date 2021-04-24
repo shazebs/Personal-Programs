c6e1139046a8eeea276d0a98fa5ce5deef521c8b
shazebs <43024685+shazebs@users.noreply.github.com>
2021-04-23 19:43:36 -0700

    Update and rename StringLink to StringLink.h

---
+++ .cpp/BattleshipProjectResourceFiles/StringLink.h
@@ -0,0 +1,58 @@
+//
+// Created by shazebs on 4/13/2021.
+//
+
+#ifndef PROJECT_1_STRINGLINK_H
+#define PROJECT_1_STRINGLINK_H
+#include <set>
+#include <iostream>
+using namespace std;
+
+
+struct StringLink
+{
+    set<string> shipPins;
+    StringLink *forward;
+    StringLink *backward;
+
+    StringLink *frontLink = nullptr;
+    StringLink *backLink = nullptr;
+
+    // Default Constructor.
+    StringLink()
+    {
+        forward = nullptr;
+        backward = nullptr;
+    }
+    // Destructor
+    ~StringLink()
+    {
+        delete &shipPins;
+        delete forward;
+        delete backward;
+    }
+
+    // StringLink Operations.
+    void pushback(StringLink *link)
+    {
+        if (!frontLink && !backLink)
+        {
+            frontLink = link;
+        }
+        else if (frontLink && !backLink)
+        {
+            frontLink->forward = link;
+            link->backward = frontLink;
+            backLink = link;
+        }
+        else if (frontLink && backLink)
+        {
+            backLink->forward = link;
+            link->backward = backLink;
+            backLink = link;
+        }
+    }
+};
+
+
+#endif //PROJECT_1_STRINGLINK_H
