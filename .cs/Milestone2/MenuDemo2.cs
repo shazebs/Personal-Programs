ce64aa26eea0d1927c10c0793c4a356b2de5deb8
shazebs <43024685+shazebs@users.noreply.github.com>
2021-11-07 01:18:38 -0700

    Update MenuDemo2.cs

--- .cs/Milestone2/MenuDemo2.cs
+++ .cs/Milestone2/MenuDemo2.cs
@@ -51,23 +51,7 @@ namespace Whiteboard
                         // Ask the user for the id number of the item to remove. 
                         // Remove it from the list. 
                         printCurrentInventory(vacations);
-                        bool condition = false;
-                        do
-                        {
-                            try
-                            {
-                                Console.WriteLine("Enter the name of the vacation you want to remove: ");
-                                string deleteMe = Console.ReadLine();
-                                deleteProduct(deleteMe.Trim(), vacations);
-                                condition = true;
-                            }
-                            catch
-                            {
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.WriteLine("You entered something invalid, try again!");
-                                Console.ResetColor();
-                            }
-                        } while (condition != true); 
+                        if (vacations.Count != 0) deleteProduct(-1, vacations); // Recursive Function Call. 
                         break;
 
                     case "edit":
@@ -75,9 +59,7 @@ namespace Whiteboard
                         // Get new values for the item. 
                         // Update the item's properties. 
                         printCurrentInventory(vacations);
-                        Console.WriteLine("Enter the name of the vacation you want to change: ");
-                        string updateMe = Console.ReadLine(); 
-                        editProduct(updateMe.Trim(), vacations);
+                        if (vacations.Count != 0) editProduct(-1, vacations); // Recursive Function Call.
                         break;
 
                     case "search":
@@ -88,7 +70,7 @@ namespace Whiteboard
                     default:
                         // Display an error message. 
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("You entered something invalid. Try again.");
+                        Console.WriteLine("\nYou entered something invalid. Try again.");
                         Console.ResetColor();
                         break; 
                 }
@@ -100,22 +82,20 @@ namespace Whiteboard
         }
         private static void CreateStartingListOfVacations(List<Vacation> vacations)
         {
-            // Create some starting values for the list. 
-            /*Thing thing1 = new Thing(99, "Banana Bender", 14.95f); 
-            Thing thing2 = new Thing(105, "Inflatable Hammer", 25.00f); 
-            Thing thing3 = new Thing(288, "Black Light", 11.55f); 
-            Thing thing4 = new Thing(422, "A Round Tuit", 0.99f);*/
-
-            DateTime dt = new DateTime(2022, 2, 7);
-
             // Instantiate new 'Vacation' objects. 
+            DateTime dt = new DateTime(2022, 2, 7);
             Vacation vacation1 = new Vacation("Bronze", "Honolulu, Hawaii", dt, 7, "1 week getaway to the beautiful island of Wakiki", 499.99f, "images/hawaii.png");
+
             dt = new DateTime(2022, 5, 8); 
-            Vacation vacation2 = new Vacation("Silver", "Paris, France", dt, 10, "10 day getaway to the romantic city of French Fries", 699.99f, "images/paris.png"); 
+            Vacation vacation2 = new Vacation("Silver", "Paris, France", dt, 10, "10 day getaway to the romantic city of French Fries", 699.99f, "images/paris.png");  
+            
+            dt = new DateTime(2022, 5, 8); 
+            Vacation vacation3 = new Vacation("Gold", "Tokyo, Japan", dt, 14, "Two week stay at the Anime Capital of the World.", 845.67f, "tokyo.png"); 
 
             // Add the instatiated objects to the list. 
             vacations.Add(vacation1);
-            vacations.Add(vacation2); 
+            vacations.Add(vacation2);
+            vacations.Add(vacation3); 
         }
         private static void printWelcome()
         {
@@ -138,7 +118,8 @@ namespace Whiteboard
                 Console.WriteLine("The inventory is empty."); 
             } else
             {
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
                 int count = 1; 
                 foreach (Vacation vacation in vacations)
                 {
@@ -193,50 +174,176 @@ namespace Whiteboard
             Vacation newVacation = new Vacation(vacationName, location, dt, daysOfTrip, description, price, photoURL);
             vacations.Add(newVacation); 
         }
-        private static void editProduct(string updateMe, List<Vacation> vacations)
+        private static void editProduct(int updateMe, List<Vacation> vacations)
         {
             // Find the thing that matches the updateMe number. 
             // Show the current values of the things to the user. 
             // Ask for a new value for name. 
             // Ask for a new value for price. 
             // Update the properties of things[updateMe]
-            foreach(Vacation vacation in vacations)
-            {
-                /*if (updateMe == vacation.id)
-                {
-                    Console.Write("Enter a new name for this thing: ");
-                    thing.name = Console.ReadLine();
-                    Console.Write("Enter a new price for this thing: $");
-                    thing.price = float.Parse(Console.ReadLine());
-                    break; 
-                }*/
 
-                if (updateMe == vacation.vacationName)
-                {
-                    Console.WriteLine("You successfully located the vacation item you want to edit."); 
-                } 
-            }
-        }
-        private static void deleteProduct(string deleteMe, List<Vacation> vacations)
-        {
-            // Find the thing that matches the deleteMe number. 
-            // Remove it from the list of things. 
-            foreach(Vacation vacation in vacations)
+            if (updateMe == -1)
             {
-                if (deleteMe == vacation.vacationName)
+                bool repeat = true;
+                do
                 {
-                    Console.WriteLine("You successfully located the vacation item you want to remove."); 
-                    vacations.Remove(vacation);
-                    Console.WriteLine("The vacation item was successfully removed from the inventory."); 
-                    break; 
+                    try
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Enter the index of the vacation item you want to edit: ");
+                        Console.ResetColor();
+                        int index = int.Parse(Console.ReadLine());
+                        while (index < 1 || index > vacations.Count)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("You entered an invalid index, try again!");
+                            Console.ResetColor();
+                            index = int.Parse(Console.ReadLine());
+                        }
+                        // At this point, a valid input has been registered by the user, so break out of do-while and move to next step. 
+                        editProduct(index, vacations);
+                        repeat = false;
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You entered something invalid, try again!");
+                        Console.ResetColor();
+                    }
+                } while (repeat != false);
+            }
+            else
+            {
+                int index = 0;
+                foreach (var vacation in vacations)
+                {
+                    if (updateMe - 1 == index)
+                    {
+                        // Edit vacation item name. 
+                        Console.ForegroundColor = ConsoleColor.Yellow; 
+                        Console.WriteLine("\n(Current vacation name = " + vacation.vacationName + ")");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("Enter vacation name edit: ");
+                        Console.ResetColor();
+                        vacation.vacationName = Console.ReadLine();
+
+                        // Edit vacation item location. 
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("(Current vacation location = " + vacation.location + ")");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("Enter vacation location edit: ");
+                        Console.ResetColor();
+                        vacation.location = Console.ReadLine();
+
+                        // Edit vacation item starting date. 
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("(Current vacation starting date = " + vacation.startingDate + ")");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("Enter vacation starting date's month (1-12): ");
+                        Console.ResetColor();
+                        int month = int.Parse(Console.ReadLine());
+                        Console.Write("Enter vacation starting date's day (1-31): ");
+                        Console.ResetColor();
+                        int day = int.Parse(Console.ReadLine());
+                        Console.Write("Enter vacation starting date's year: ");
+                        Console.ResetColor();
+                        int year = int.Parse(Console.ReadLine());
+                        DateTime dt = new DateTime(year, month, day);
+                        vacation.startingDate = dt; 
+
+                        // Edit vacation item days of trip. 
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("(Current vacation starting date = " + vacation.daysOfTrip + ")");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("Enter vacation days of trip edit: ");
+                        Console.ResetColor();
+                        vacation.daysOfTrip = int.Parse(Console.ReadLine());
+
+                        // Edit vacation item description. 
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("(Current vacation description = " + vacation.description + ")");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("Enter vacation description edit: ");
+                        Console.ResetColor();
+                        vacation.description = Console.ReadLine();
+
+                        // Edit vacation item price. 
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("(Current vacation price = $" + vacation.price + ")");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("Enter vacation price edit: ");
+                        Console.ResetColor();
+                        vacation.price = float.Parse(Console.ReadLine());
+
+                        // Edit vacation photo URL. 
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("(Current vacation photo URL = " + vacation.photoURL + ")");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("Enter vacation photo URL edit: ");
+                        Console.ResetColor();
+                        vacation.photoURL = Console.ReadLine();
+
+                        Console.ResetColor();
+                        break; // to exit foreach loop. 
+                    }
+                    index++;
                 }
             }
         }
-        /*private static List<string> seachForItems(string searchPhrase, List<string> mythings)
+        public static void deleteProduct(int deleteMe, List<Vacation> vacations)
         {
-            // loop through the list or products 
+            // Find the thing that matches the deleteMe number. 
+            // Remove it from the list of things. 
 
-            // if a partial 
-        }*/
+            if (deleteMe == -1)
+            {
+                bool condition = false;
+                do
+                {
+                    try
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("Enter the index of the vacation you want to remove: ");
+                        Console.ResetColor();
+
+                        deleteMe = int.Parse(Console.ReadLine());
+
+                        while (deleteMe < 1 || deleteMe > vacations.Count)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("You entered an invalid index, try again!");
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.Write("Enter the index of the vacation you want to remove: ");
+                            Console.ResetColor();
+                            deleteMe = int.Parse(Console.ReadLine());
+                        }
+                        deleteProduct(deleteMe, vacations);
+                        condition = true;
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You entered something invalid, try again!");
+                        Console.ResetColor();
+                    }
+                } while (condition != true); 
+            }
+            else
+            {
+                int index = 0; 
+                foreach (var vacation in vacations)
+                {
+                    if (deleteMe-1 == index)
+                    {
+                        vacations.Remove(vacation);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("The vacation item was successfully located and removed from the inventory.");
+                        Console.ResetColor();
+                        break; 
+                    }
+                    index++; 
+                }
+            }
+        }
     }
 }
