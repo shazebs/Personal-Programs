d1ab33daeed7c582ea3a09d839a37848680818fd
shazebs <43024685+shazebs@users.noreply.github.com>
2022-01-22 03:35:38 -0700

    Update Program.cs

--- .cs/ChessBoardConsoleApp/Program.cs
+++ .cs/ChessBoardConsoleApp/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// using ChessBoardModel2;
+using ChessBoardClasses;
 
-namespace ChessBoardConsoleApp
+namespace ChessBoard
 {
 	class Program
 	{
@@ -19,34 +19,35 @@ namespace ChessBoardConsoleApp
 			printBoard(myBoard);
 
 			// ask the user for an x andy coordinate where we will place a piece
-			Cell currentCell = setCurrentCell(); 
-			currentCell.CurrentlyOccupied = true; 
+			Cell currentCell = setCurrentCell();
+			currentCell.CurrentlyOccupied = true;
 
 			// calculate all legal moves for that piece
 
 			// print the chess board. Use an x for occupied square. Use a + for legal move. Use . for empty cell.
 			printBoard(myBoard);
-			
+
 			// wait for another enter key press before ending the program.
-			Console.ReadLine(); 
+			Console.Write("\nExit Game ");
+			Console.ReadLine();
 		}
-		
+
 		// functions 
-		
+
 		// set current cell function 
 		private static Cell setCurrentCell()
 		{
 			// get x and y coordinates from the user. return a cell location.
-			Console.WriteLine ( "Enter the current row number" ); 
-			int currentRow = int.Parse( Console.ReadLine() );
-			Console.WriteLine ( "Enter the current row number" ); 
-			int currentCol = int.Parse( Console.ReadLine() ); 
-			
-			myBoaord.theGrid [ currentRow, currentCol ]; 
+			Console.WriteLine("Enter the current row number");
+			int currentRow = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter the current row number");
+			int currentCol = int.Parse(Console.ReadLine());
+
+			return myBoard.theGrid[currentRow, currentCol];
 		}
 
-		
-		
+
+
 		// print boad (function) 
 		private static void printBoard(Board myBoard)
 		{
@@ -70,7 +71,7 @@ namespace ChessBoardConsoleApp
 
 			Console.WriteLine("================================================");
 		}
-		
-		
+
+
 	}
 }
