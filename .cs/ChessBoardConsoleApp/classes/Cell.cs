fc20a64683bb5c593c9a0fd914edb472093db9e3
shazebs <43024685+shazebs@users.noreply.github.com>
2022-01-22 03:36:37 -0700

    Update Cell.cs

--- .cs/ChessBoardConsoleApp/classes/Cell.cs
+++ .cs/ChessBoardConsoleApp/classes/Cell.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChessBoardConsoleApp
+namespace ChessBoard
 {
 	public class Cell
 	{
+		// properties 
 		public int RowNumber { get; set; }
 		public int ColumnNumber { get; set; }
 		public bool CurrentlyOccupied { get; set; }
 		public bool LegalNextMove { get; set; }
 
+		// constructor 
 		public Cell(int x, int y)
 		{
 			RowNumber = x;
