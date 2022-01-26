0824d489351de8f75c71fe00de8eacc95e7e687c
shazebs <43024685+shazebs@users.noreply.github.com>
2022-01-26 13:42:33 -0700

    Create Armor.java

---
+++ .java/WoW_Armory/src/app/Armor.java
@@ -0,0 +1,28 @@
+package app;
+
+//armor
+public class Armor extends SalableProduct
+{
+	 // properties
+	 private String name;
+	 private String description;
+	 private double price;
+	 private int quantity;
+	
+	 // constructor(s)
+	 public Armor(){}
+	 public Armor(String a, String b, double c, int d)
+	 {
+	     /*name = a;
+	     description = b;
+	     price = c;
+	     quantity = d;*/
+	     super();
+	 }
+
+	 // class methods
+	
+	 // setters
+	
+	 // getters
+}
