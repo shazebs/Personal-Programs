7bd9bad11a34305396ce5f9657b60b2478cc9c02
shazebs <43024685+shazebs@users.noreply.github.com>
2022-01-26 13:42:58 -0700

    Create Health.java

---
+++ .java/WoW_Armory/src/app/Health.java
@@ -0,0 +1,28 @@
+package app;
+
+//health
+public class Health extends SalableProduct
+{
+ // properties
+ private String name;
+ private String description;
+ private double price;
+ private int quantity;
+
+ // constructor(s)
+ public Health(){}
+ public Health(String a, String b, double c, int d)
+ {
+     /*name = a;
+     description = b;
+     price = c;
+     quantity = d;*/
+     super();
+ }
+
+ // class methods
+
+ // setters
+
+ // getters
+}
