5737b727d1b076731913cf5a443fed8a85e404b6
shazebs <43024685+shazebs@users.noreply.github.com>
2022-01-29 22:41:48 -0700

    Create Cell.cs

---
+++ .cs/MineSweeper/Minesweeper_pt2/classes/Cell.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_1
+{
+    class Cell
+    {
+        // Properties 
+        public int row { get; set; }
+        public int col { get; set; }
+        public bool isVisited { get; set; }
+        public bool isLive { get; set; }
+        public int liveNeighbors { get; set; }
+        public bool hasFlag { get; set; }
+
+        // Constructors 
+        public Cell() 
+        {
+            this.col = -1;
+            this.row = -1;
+            this.isVisited = false;
+            this.isLive = false;
+            this.liveNeighbors = 0;
+            this.hasFlag = false; 
+        }
+
+        // Constructor 2
+        public Cell(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+            this.isVisited = false;
+            this.isLive = false;
+            this.liveNeighbors = 0;
+            this.hasFlag = false; 
+        }
+
+        // Getters
+        public int getRow() { return this.row; }
+        public int getCol() { return this.col; }
+        public bool getIsVisited() { return isVisited; }
+        public bool getIsLive() { return isLive; }
+        public int getLiveNeighbors() { return liveNeighbors; }
+        public bool getHasFlag() { return hasFlag; }
+
+        // Setters
+        public void setRow(int row) { this.row = row; }
+        public void setCol(int col) { this.col = col; }
+        public void setIsVisited(bool x) { this.isVisited = x; }
+        public void setIsLive(bool x) { this.isLive = x; }
+        public void setLiveNeighbors(int x) { this.liveNeighbors = x; }
+        public void setHasFlag(bool flag) { this.hasFlag = flag; }
+    }
+}
