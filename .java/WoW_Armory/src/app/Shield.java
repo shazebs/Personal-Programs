bf3a20e50a5db749ecdeeb8bff75531b0db7c157
shazebs <43024685+shazebs@users.noreply.github.com>
2022-01-31 00:23:45 -0700

    Update and rename Weapon.java to Shield.java

---
+++ .java/WoW_Armory/src/app/Shield.java
@@ -0,0 +1,13 @@
+package app;
+
+//weapon
+public class Shield extends SalableProduct
+{
+	 // properties (inherited) 
+	
+	 // constructor(s)
+	 public Shield() { super(); }
+	
+	 // class methods
+	
+}
