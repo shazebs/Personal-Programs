6a10e4c7d995184af60626eae680a44bc18e9c4b
shazebs <43024685+shazebs@users.noreply.github.com>
2022-02-05 01:03:07 -0700

    Update SalableProduct.java

--- .java/WoW_Armory/src/app/SalableProduct.java
+++ .java/WoW_Armory/src/app/SalableProduct.java
@@ -8,6 +8,7 @@ public class SalableProduct
     private String description;
     private double price;
     int iLvl;
+    String type; 
 
     // constructor(s)
     public SalableProduct(){
@@ -15,13 +16,15 @@ public class SalableProduct
         description = "";
         price = 0.00;
         iLvl = 0;
+        type = ""; 
     }
-    public SalableProduct(String a, String b, double c, int d)
+    public SalableProduct(String a, String b, double c, int d, String e)
     {
         name = a;
         description = b;
         price = c;
         iLvl = d;
+        type = e; 
     }
 
     // class methods
@@ -34,6 +37,7 @@ public class SalableProduct
     public void setDescription(String x) { this.description = x; }
     public void setPrice(double x) { this.price = x; }
     public void setItemLevel(int x) { this.iLvl = x; }
+    public void setType(String x) { this.type = x; }
 
     /**
      * 
@@ -43,12 +47,13 @@ public class SalableProduct
     public String getDescription() { return description; }
     public double getPrice() { return price; }
     public int getItemLevel() { return iLvl; }
+    public String getType() { return type; }
 
     // toString
     @Override
     public String toString()
     {
-        return String.format("%s\n%s\n$%,.2f\niLvl %d\n",
-                name, description, price, iLvl);
+        return String.format("%s\n%s\n%s\n$%,.2f\niLvl %d\n",
+                name, type, description, price, iLvl);
     }
 }
