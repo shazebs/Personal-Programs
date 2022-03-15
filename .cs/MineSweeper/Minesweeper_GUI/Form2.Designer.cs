0d12081f796dc5bb8df88a7193606ec5f8be1424
shazebs <43024685+shazebs@users.noreply.github.com>
2022-03-14 21:45:56 -0700

    Add files via upload

---
+++ .cs/MineSweeper/Minesweeper_GUI/Form2.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Minesweeper_GUI
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioEasy = new System.Windows.Forms.RadioButton();
+            this.radioMedium = new System.Windows.Forms.RadioButton();
+            this.radioHard = new System.Windows.Forms.RadioButton();
+            this.btn_startgame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Level Difficulty";
+            // 
+            // radioEasy
+            // 
+            this.radioEasy.AutoSize = true;
+            this.radioEasy.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioEasy.Location = new System.Drawing.Point(130, 105);
+            this.radioEasy.Name = "radioEasy";
+            this.radioEasy.Size = new System.Drawing.Size(86, 30);
+            this.radioEasy.TabIndex = 1;
+            this.radioEasy.TabStop = true;
+            this.radioEasy.Text = "Easy";
+            this.radioEasy.UseVisualStyleBackColor = true;
+            // 
+            // radioMedium
+            // 
+            this.radioMedium.AutoSize = true;
+            this.radioMedium.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioMedium.Location = new System.Drawing.Point(130, 186);
+            this.radioMedium.Name = "radioMedium";
+            this.radioMedium.Size = new System.Drawing.Size(115, 30);
+            this.radioMedium.TabIndex = 2;
+            this.radioMedium.TabStop = true;
+            this.radioMedium.Text = "Medium";
+            this.radioMedium.UseVisualStyleBackColor = true;
+            // 
+            // radioHard
+            // 
+            this.radioHard.AutoSize = true;
+            this.radioHard.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioHard.Location = new System.Drawing.Point(130, 267);
+            this.radioHard.Name = "radioHard";
+            this.radioHard.Size = new System.Drawing.Size(84, 30);
+            this.radioHard.TabIndex = 3;
+            this.radioHard.TabStop = true;
+            this.radioHard.Text = "Hard";
+            this.radioHard.UseVisualStyleBackColor = true;
+            // 
+            // btn_startgame
+            // 
+            this.btn_startgame.BackColor = System.Drawing.Color.YellowGreen;
+            this.btn_startgame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_startgame.Location = new System.Drawing.Point(93, 333);
+            this.btn_startgame.Name = "btn_startgame";
+            this.btn_startgame.Size = new System.Drawing.Size(193, 61);
+            this.btn_startgame.TabIndex = 4;
+            this.btn_startgame.Text = "Start Game";
+            this.btn_startgame.UseVisualStyleBackColor = false;
+            this.btn_startgame.Click += new System.EventHandler(this.btn_startgame_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 425);
+            this.Controls.Add(this.btn_startgame);
+            this.Controls.Add(this.radioHard);
+            this.Controls.Add(this.radioMedium);
+            this.Controls.Add(this.radioEasy);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game Initialization";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioEasy;
+        private System.Windows.Forms.RadioButton radioMedium;
+        private System.Windows.Forms.RadioButton radioHard;
+        private System.Windows.Forms.Button btn_startgame;
+    }
+}
