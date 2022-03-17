8b5c1e72da348fbc4a086976422fee067dfd2d95
shazebs <43024685+shazebs@users.noreply.github.com>
2022-03-17 03:41:19 -0700

    Update dbconnect.php

--- web-dev-projects/dbconnect.php
+++ web-dev-projects/dbconnect.php
@@ -1,4 +1,26 @@
 <?php
 
+// database connection details 
+$serverName = "localhost";
+$userName = ""; 
+$password = ""; 
+$dbName = ""; 
+
+// try to connect to database 
+$dbc = mysqli_connect($servername, $username, $password, $dbname) 
+    OR die ( mysqli_connect_error() ); 
+    mysqli_set_charset($dbc, 'utf8'); 
+
+// print a successful database response 
+if ($dbc->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+else {
+    echo '<h3>Successfully connected to Database : ' . $dbName . '</h3>';
+}
+
+
+// Close the connection
+// $conn->close();
 
 ?>
