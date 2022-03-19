5b00d156ceaf97589c5714ca682696fe69d189c4
shazebs <43024685+shazebs@users.noreply.github.com>
2022-03-19 08:47:27 -0700

    Update index.php

--- .html/LopesUndergroundChat/index.php
+++ .html/LopesUndergroundChat/index.php
@@ -40,6 +40,8 @@ if ($chat_text && $chat_user)
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    
+    <link rel="stylesheet" href="css/underground.css">
 
 </head>
 <!-- end of head section --> 
