b6fb496fa6309b014443b0d893efba78aa997b3b
shazebs <43024685+shazebs@users.noreply.github.com>
2022-03-19 09:26:01 -0700

    Update chatPostHandler.php

--- .html/LopesUndergroundChat/php/chatPostHandler.php
+++ .html/LopesUndergroundChat/php/chatPostHandler.php
@@ -1,46 +1,31 @@
 <?php
+require('dbconnect.php'); 
 
-require('../php/dbconnect.php'); 
-
-/* retrieve POST variables */ 
 $chat_text = $_POST["chat"];
-$chat_user = $_POST["username"];
+$chat_user = $_POST["username"]; 
 
-/* insert new chat into database */
-$sql = "INSERT INTO `chats` (`chat_id`, `chat_text`, `chat_date`, `chat_user`) VALUES (NULL, '".$chat_text."', current_timestamp(), '".$chat_user."');";
-$result = mysqli_query($db, $sql);
-
-/* if success */ 
-if ($result){
-    echo 'Successful Insert!<br>'; 
-}
-/* if fail */ 
-else {
-    echo 'Unsuccessful Insert.<br>'; 
+if ($chat_text && $chat_user)
+{
+    $sql = "INSERT INTO `chats` (`chat_id`, `chat_text`, `chat_date`, `chat_user`) VALUES (NULL, '".$chat_text."', current_timestamp(), '".$chat_user."')"; 
+    $result = mysqli_query($db, $sql); 
 }
 
-/* close the connection */ 
-$db->close();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Underground.css</title>
-    <link rel="stylesheet" src="">
-    <script>
-        const username = <?php $chat_user ?>; 
-    </script>
+    <title>The Underground</title>
+    <link rel="stylesheet" href="../phpdocs/css/underground.css">
 </head>
-    
-    
+
+
 <body>
-    <h1>Welcome to the Underground, <script>username</script></h1>
+    <h1>Welcome to the Underground, <?php echo $chat_user ?>.</h1>
 </body>
 
-</html>
 
+</html>
