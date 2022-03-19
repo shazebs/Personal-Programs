cd362b82c0dd17b3b760ae66627a40dba8c1957f
shazebs <43024685+shazebs@users.noreply.github.com>
2022-03-19 08:27:31 -0700

    Update chatPostHandler.php

--- .html/LopesUndergroundChat/php/chatPostHandler.php
+++ .html/LopesUndergroundChat/php/chatPostHandler.php
@@ -3,17 +3,18 @@
 require('../php/dbconnect.php'); 
 
 /* retrieve POST variables */ 
-$chat_text = "";
-$chat_user = ""; 
+$chat_text = $_POST["chat"];
+$chat_user = $_POST["username"];
 
-/* insert a new chat into the database */
+/* insert new chat into database */
 $sql = "INSERT INTO `chats` (`chat_id`, `chat_text`, `chat_date`, `chat_user`) VALUES (NULL, '".$chat_text."', current_timestamp(), '".$chat_user."');";
 $result = mysqli_query($db, $sql);
 
-/* insertion success message */ 
+/* if success */ 
 if ($result){
     echo 'Successful Insert!<br>'; 
 }
+/* if fail */ 
 else {
     echo 'Unsuccessful Insert.<br>'; 
 }
