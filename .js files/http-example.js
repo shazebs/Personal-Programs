9401e32e28ce41159cc53e1ec66d93dec80b581a
shazebs <43024685+shazebs@users.noreply.github.com>
2022-03-19 06:52:27 -0700

    Update http-example.js

--- .js files/http-example.js
+++ .js files/http-example.js
@@ -1,7 +1,16 @@
-const http = require('http'); 
+const { get } = require('https'); 
 
-http.request('www.google.com', (res) => {
-  res.on('data', (chunk) => {
-    console.log(`Data chunk ${chunk}`); 
-  }); 
+/* http request example */
+const req = get('https://www.shazebs.com', (res) => {
+
+    // data call  
+    res.on('data', (chunk) => {
+        console.log(`Data chunk ${chunk}`); 
+    }); 
+
+    // end call  
+    res.on('end', () => {
+        console.log('No more data'); 
+    }); 
+
 });
