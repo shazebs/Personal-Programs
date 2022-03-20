174cc00755dae1e97b49ec03eab5170f6fd819e4
shazebs <43024685+shazebs@users.noreply.github.com>
2022-03-20 03:01:43 -0700

    Update dbconnect.php

--- .html/LopesUndergroundChat/php/dbconnect.php
+++ .html/LopesUndergroundChat/php/dbconnect.php
@@ -2,9 +2,9 @@
 
 /* set up a database connection */ 
 $server = "localhost";
-$username = "u300175299_sora";
-$password = "Glydeub40"; 
-$database = "u300175299_dbsora"; 
+$username = "";
+$password = ""; 
+$database = ""; 
 $db = new mysqli($server, $username, $password, $database);
 if (!$db) {
   die("Sorry we failed to connect: ". mysqli_connect_error()); }
