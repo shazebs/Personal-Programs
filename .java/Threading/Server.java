b75c48c67ef83a79bf3bae8a6bbb0913c575ae92
shazebs <43024685+shazebs@users.noreply.github.com>
2022-04-06 23:57:28 -0700

    Update Server.java

--- .java/Threading/Server.java
+++ .java/Threading/Server.java
@@ -1,5 +1,5 @@
+package server;
 
-// library imports
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -19,8 +19,7 @@ public class Server
     static Scanner cin = new Scanner(System.in);
 
     // Function to start the server and wait for connections on the specified port.
-    static public void start(int port) throws IOException
-    {
+    static public void start(int port) throws IOException {
         // Wait for a client connection.
         System.out.println("\nWaiting for a Client connection to this server....");
 
@@ -40,83 +39,10 @@ public class Server
         out.println("You are online!");
 
         // Admin User can now send commands to the store.
-        if(!SendCommands()) {
-            System.out.println("Sending commands has ended.");
-        }
 
         // all responses have been received.
         System.out.println("Server is now shutting down...");
     }
-
-
-    // Receive commands for Client-side class.
-    static public void ReceiveCommands() throws IOException {
-        // function properties
-        String command = "";
-
-        // function task
-        while ( command != "end" ) {
-            System.out.println( "Enter a command:\n" );
-            switch ( cin.nextLine().trim() )
-            {
-                case "u":
-                case "U":
-                    System.out.println( "Command: U" );
-                    break;
-
-                case "r":
-                case "R":
-                    System.out.println( "Command: R" );
-                    break;
-
-                case "end":
-                case "END":
-                    System.out.println( "END of receiving commands." );
-                    break;
-
-                default:
-                    System.out.println( "That command does not exist." );
-                    break;
-            }
-        }
-    }
-
-
-    // Send Commands
-    static public boolean SendCommands() {
-        boolean on = true;
-        do {
-            System.out.print("Enter a command: ");
-            switch ( cin.nextLine().trim() )
-            {
-                case "u":
-                case "U":
-                    System.out.println("Command: U");
-                    out.println("Command: U was called from Admin User.");
-                    break;
-
-                case "r":
-                case "R":
-                    System.out.println("Command: R");
-                    out.println("Command: R was called from Admin User.");
-                    break;
-
-                case "end":
-                case "END":
-                    System.out.println("END of sending commands.");
-                    out.println("END of receiving commands from Admin User.");
-                    on = false;
-                    break;
-
-                default:
-                    System.out.println("That command does not exist.");
-                    break;
-            }
-            System.out.println();
-        } while (on);
-        return on;
-    }
-
 
     // this function closes any open utilities before ending program.
     static public void cleanup() throws IOException {
@@ -141,36 +67,6 @@ public class Server
         ServerThread ServerThread = new ServerThread("Server Thread");
         Thread serverThread = new Thread(ServerThread);
         serverThread.start();
-
         cleanup();
     }
-
-} // end of Server class.
-
-/*
-<-- Additional Assignment Notes -->
-
-1. Ability to send Administration commands over the local network from an Admin User to the Store Front Application --
-
-2. Command: U, will update the Store Inventory Management System with new Salable Products.
-    The data payload will be a JSON string of Salable Products. --
-
-3. Command: R, will return all of the Salable Products from the Store Inventory Management System in JSON format. --
-
-4. The Administration Service should be run in the background,
-    such that it does not impact the User while interacting with the Store Front Application,
-    and should leverage the File Server to read and write JSON inventory data files.
-
-5. Update UML
-
-6. Update FLOWCHART
-
-7. Create JavaDocs
-
-8. Create a Screencast Presentation
-
-9. Upload to Github
-
-10. Submit the assignment
-
- */
+}
